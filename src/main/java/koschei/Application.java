e1d0900121e7317cbbc0a0408bfbee04b61142bd
OlegB <oleg89115498683@yandex.ru>
2025-04-03 21:50:53 +0300

    f3

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -3,7 +3,11 @@ package koschei;
 import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
